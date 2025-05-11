aa919f20fb5448ff070689795e990f3711735cf7
Denis Neves <nevesdenis35@gmail.com>
2025-05-11 18:35:50 -0300

    Nem sei mais o que eu fiz aqui

---
+++ js/shopCart.js
@@ -0,0 +1,34 @@
+const buttonContainer = document.querySelector(".container-card");
+const spanCount = document.createElement("span");
+const cart = document.querySelector(".cart i");
+
+let count = 0;
+
+buttonContainer.addEventListener("click", (event) => {
+  if (event.target.closest("button.add")) {
+    count++;
+    updateCardContent();
+  }
+
+  if (event.target.closest("button.remove")) {
+    if (count > 0) {
+      count--;
+      updateCardContent();
+    }
+  }
+  //funciona fudido
+  console.log("Clicou em:", event.target);
+});
+
+function updateCardContent() {
+  if (count <= 0) {
+    if (cart.contains(spanCount)) {
+      cart.removeChild(spanCount);
+    }
+  } else {
+    spanCount.textContent = count;
+    if (!cart.contains(spanCount)) {
+      cart.appendChild(spanCount);
+    }
+  }
+}
